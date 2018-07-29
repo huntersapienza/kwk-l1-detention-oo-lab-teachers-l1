1a8216f27ef890380aadd5012cc2b1fcc77f47a3
Hunter Sapienza <sapienzahunter@gmail.com>
2018-07-29 16:44:46 +0000

    automatically backed up by learn

--- detention.rb
+++ detention.rb
@@ -1,9 +1,10 @@
 #Code your detention class here
 class Detention
-  attr_accessor :location, :time, :grumpy_teacher, :activity
+  attr_accessor :location, :time, :grumpy_teacher, :activity, :students
   
-  def initialize(activity = "collective punishment")
+  def initialize(activity = "collective punishment", students = [])
     @activity = activity
+    @students = students
   end
   
 end
