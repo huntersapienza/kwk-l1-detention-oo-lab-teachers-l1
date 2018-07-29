5fdc21ec7e1ef6f5611bb7902a591996c6896663
Hunter Sapienza <sapienzahunter@gmail.com>
2018-07-29 16:43:51 +0000

    automatically backed up by learn

--- detention.rb
+++ detention.rb
@@ -2,6 +2,7 @@
 class Detention
   attr_accessor :location, :time, :grumpy_teacher, :activity
   
-  def initialize
-    @activity = "collective punishment"
+  def initialize(activity = "collective punishment")
+    @activity = activity
+  end
 end
